49b520b7bf8d387f3e2d633309cc4de3bbe2e6bd
Fonzy57 <traben@hotmail.fr>
2024-08-12 14:09:23 +0200

    Add top button animation, scroll and remove fade in animation under 1435 px

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+  /* ELEMENTS */
+  const topButton = document.getElementById("top-button");
 
   /* FUNCTION TO SEE IF ELEMENT IS IN VIEWPORT */
   const isElementInViewport = (element) => {
@@ -15,6 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /* ADD CSS VISIBLE IF THE ELEMENT IS IN VIEWPORT */
   const showElement = () => {
+    /* DON'T SHOW ANIMATION ON MOBILE AND TABLET */
+    if (window.innerWidth < 1435) {
+      return;
+    }
+
     const elements = document.querySelectorAll(".fade-in");
     elements.forEach((element) => {
       if (isElementInViewport(element)) {
@@ -23,6 +30,31 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
-  window.addEventListener("scroll", showElement);
+  /* FADE IN ANIMATION FOR SECTIONS */
   showElement();
+  window.addEventListener("scroll", showElement);
+  window.addEventListener("resize", showElement);
+
+  /* TOP BUTTON */
+  const toggleTopButton = () => {
+    if (window.scrollY > 750) {
+      topButton.classList.add("visible");
+    } else {
+      topButton.classList.remove("visible");
+    }
+  };
+
+  /* CHECK POSITION FROM USER WHEN PAGE IS LOADED */
+  toggleTopButton();
+
+  /* SHOW BUTTON WHEN USER SCROLL IN THE PAGE */
+  window.addEventListener("scroll", toggleTopButton);
+
+  /* GO TO TOP IF USER CLICK ON TOP BUTTON */
+  topButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
 });
